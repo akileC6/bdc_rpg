dd36558e76e541aaeba0b87fb101431a4b1148dd
Akile <orucakile4@gmail.com>
2025-08-06 15:20:54 +0200

    test

---
+++ test.ts
@@ -0,0 +1,4 @@
+test('Connexion + navigation vers Rooms', async ({ page }) => {
+  const navigation = new NavigationHelper(page);
+  const loginPage = new LoginPage(page); // tu n’as plus besoin de passer baseURL ici
+  const helper= new Helper(page);
